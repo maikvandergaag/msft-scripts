7ab59a07c04d1a12038dc855cb4e996512eb11f9
Maik van der Gaag <maik@familie-vandergaag.nl>
2017-09-05 11:48:01 +0200

    new commit

---
+++ Export/export-azureservices.ps1
@@ -0,0 +1,31 @@
+﻿###################################################################################
+##
+## PowerShell script for exporting Azure Resources wihtin a subscription.
+## Creator: Maik van der Gaag
+##
+###################################################################################
+
+
+Login-AzureRmAccount
+$path = Read-Host "Enter the full path to save the export file to"
+
+$subsciptions = Get-AzureRmSubscription
+
+Write-Host "Subscriptions"
+Write-Host "--------------"
+foreach($sub in $subsciptions){
+
+    Write-Host ($sub | Select -ExpandProperty "Name")
+}
+
+Write-Host ""
+
+$name = Read-Host "Please enter the subscription names for which you want to export the Azure Services devided by (,)"
+
+$names = $name.Split(",");
+
+foreach($subName in $names){
+    Write-Host "Exporting Subscription:" -ForegroundColor Green
+    Set-AzureRmContext -SubscriptionName $subName
+    Get-AzureRmResource | Select-Object Name, ResourceType, ResourceGroupName, SubscriptionId | Export-Csv -Path $path -Encoding ascii -NoTypeInformation -Append
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Scripts
+
+This repository contains Scripts for different actions
+
+
+### Export Azure Services
+
+Script for exporting Azure Services to a CSV file.
+
+[Export](\Export\README.MD)
