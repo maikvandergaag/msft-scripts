10e3fb50b77752551d25354603c908c7a3aa0d7c
Maik van der Gaag <maik@familie-vandergaag.nl>
2024-11-26 20:46:08 +0100

    Added script for migration Azure DevOps Feed

---
+++ Migrate-Feed/Migrate-Packages.ps1
@@ -0,0 +1,92 @@
+param (
+    [string]$organization,
+    [string]$project,
+    [string]$sourceFeed,
+    [string]$destinationFeed,
+    [string]$localFolder,
+    [string]$pat
+)
+
+# Base64 encode the PAT
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+
+# Function to get packages from a feed
+function Get-Packages {
+    param (
+        [string]$feed
+    )
+    $url = "https://feeds.dev.azure.com/$organization/$project/_apis/packaging/feeds/$feed/packages?api-version=6.0-preview.1&includeAllVersions=true"
+    $response = Invoke-RestMethod -Uri $url -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    return $response.value
+}
+
+# Function to download all package files including their versions from a feed
+function Get-PackagesFiles {
+    param (
+        [string]$feed,
+        [string]$destinationFolder
+    )
+
+    $feedUri = "https://pkgs.dev.azure.com/$organization/$project/_packaging/$feed/pypi/simple/"
+    $packages = Get-Packages -feed $feed
+
+    foreach ($package in $packages) {
+        foreach ($version in $package.versions) {
+            $packageName = $package.normalizedName
+
+            Write-Host "Downloading package: $packageName==$($version.version)" -ForegroundColor Green
+            pip download "$packageName==$($version.version)" --no-deps --dest $destinationFolder --ignore-requires-python --index-url $feedUri
+        }
+    }
+}
+
+# Function to publish packages using twine
+function Publish-Packages {
+    param (
+        [string]$sourceFolder,
+        [string]$destination
+    )
+
+    $feed = "https://pkgs.dev.azure.com/$organization/_packaging/$destination/pypi/upload/"
+
+    $twinePath = "twine"  # Ensure twine is installed and available in the PATH
+
+    $packageFiles = Get-ChildItem -Path $sourceFolder
+
+    foreach ($packageFile in $packageFiles) {
+        $filePath = $packageFile.FullName
+
+        Write-host "Publishing $filePath to $feed"
+        & $twinePath upload --repository-url $feed -u x -p $pat $filePath 
+    }
+}
+
+# Function to update package information
+function Update-PackageInfo {
+    param (
+        [string]$feed,
+        [string]$packageName,
+        [string]$packageVersion,
+        [string]$newDescription,
+        [string[]]$newTags
+    )
+
+    $url = "https://feeds.dev.azure.com/$organization/$project/_apis/packaging/feeds/$feed/pypi/packages/$packageName/versions/$packageVersion?api-version=6.0-preview.1"
+    $body = @{
+        description = $newDescription
+        tags = $newTags
+    } | ConvertTo-Json
+
+    try {
+        $response = Invoke-RestMethod -Uri $url -Method Patch -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo); "Content-Type"="application/json"} -Body $body
+        Write-Output "Package information updated successfully: $($response | ConvertTo-Json -Depth 10)"
+    } catch {
+        Write-Error "Failed to update package information for $packageName version $packageVersion. Error: $_"
+    }
+}
+
+# Main script execution
+Get-PackagesFiles -feed $sourceFeed -destinationFolder $localFolder
+Publish-Packages -sourceFolder $localFolder -destination $destinationFeed
+
+Write-Output "Packages have been successfully copied to the destination feed."
--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 This repository contains Scripts for different actions
 
 
+### Migrate Azure DevOps Feed
+
+Script for migrating a Azure DevOps Artifact feed to a organizational  feed.
+
+[Migrate Azure DevOps Feed](Migrate-Feed)
+
 ### Export Azure Services
 
 Script for exporting Azure Services to a CSV file.
